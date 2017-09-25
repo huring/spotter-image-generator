1b8cb1596556a3abe1bfe4727dc6b15f7c114de4
Lars Huring <lars.huring@gmail.com>
2017-09-26 01:28:19 +0200

    Comments

--- js/lib/helpers.js
+++ js/lib/helpers.js
@@ -2,13 +2,12 @@ const fs = require("fs");
 
 // Return random full Int
 exports.randomInt = function(max) {
-
     min = Math.ceil(0);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - 0)) + 0;
-
 }
 
+// Generate random Alphanumeric string with specified length
 exports.randomAlphaNumeric = function(length) {
     return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
 }
@@ -17,11 +16,9 @@ exports.rmDir = function(dirPath, removeSelf) {
     if (removeSelf === undefined)
       removeSelf = true;
 
-    
     try { var files = fs.readdirSync(dirPath); }
     catch(e) {
-    
-        console.log(e);
+        console.error(e);
         return; 
     }
     if (files.length > 0)
